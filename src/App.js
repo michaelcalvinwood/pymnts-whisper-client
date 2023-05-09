5e007c68ff51e2f5d942973b14f30276a7071870
Michael Wood <michaelwood33311@icloud.com>
2023-05-09 19:19:30 -0400

    added chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import logo from './logo.svg';
+import { io } from 'socket.io-client';
 import './App.css';
+import { Heading } from '@chakra-ui/react';
 
 function App() {
-  
-  setTimeout(() => {
-    if (!window.socketConnection) window.socketConnection = io(`https://node.pymnts.com:6400`);
-  }, 2000)
+
+  if (!window.socketConnection) window.socketConnection = io(`https://node.pymnts.com:6400`);
+    
+
 
   
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading>PYMNTS Whisper</Heading>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme as proTheme } from '@chakra-ui/pro-theme'
+import { extendTheme, theme as baseTheme, ColorModeScript } from '@chakra-ui/react'
+
+export const theme = extendTheme(
+  {
+    colors: { ...baseTheme.colors, brand: baseTheme.colors.blue },
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+  proTheme,
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
-  </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
