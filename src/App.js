bfa4cc4bbd81696ca2624845daf2f4f18e2c6673
Michael Wood <michaelwood33311@icloud.com>
2023-05-09 19:33:49 -0400

    added handling messages via socket

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import logo from './logo.svg';
 import { io } from 'socket.io-client';
 import './App.css';
-import { Heading } from '@chakra-ui/react';
+import { Alert, AlertIcon, Container, Heading } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [alertMessage, setAlertMessage] = useState('');
+  const [alertStatus, setAlertStatus] = useState('error');
 
-  if (!window.socketConnection) window.socketConnection = io(`https://node.pymnts.com:6400`);
+  const message = (msg, status) => {
+    setAlertMessage(msg);
+    setAlertStatus(status);
+  }
+
+  if (!window.socketConnection) {
+    window.socketConnection = io(`https://node.pymnts.com:6400`);
     
+    window.socketConnection.on('message', (msg) => message(msg, 'success'));
+    window.socketConnection.on('error', (msg) => message(msg, 'error'));
+  }
 
 
-  
+  useEffect(() => {
+    window.socketConnection.emit('url', 'the url')
+  }, []) 
 
   return (
     <div className="App">
       <Heading>PYMNTS Whisper</Heading>
+      <Container>
+      <Alert status={alertStatus} marginBottom={'0'} visibility={alertStatus && alertMessage ? 'visible' : 'hidden'}>
+          <AlertIcon />
+          {alertMessage}
+      </Alert>
+      </Container>
     </div>
   );
 }
