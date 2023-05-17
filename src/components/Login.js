f73cbe1f6a470a4bb2aabb2c72240e99238ca68c
Michael Wood <michaelwood33311@icloud.com>
2023-05-17 11:08:58 -0400

    added front-end for Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
-import { Container, Heading } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Input, Text } from '@chakra-ui/react';
+import { set } from 'lodash';
 import React, { useState } from 'react'
 
 function Login() {
@@ -8,6 +9,15 @@ function Login() {
   return (
     <Container>
         <Heading size='sm' textAlign='center'>Login</Heading>
+        <Box display="flex" alignItems={'center'} margin="1rem auto .5rem auto" width="fit-content" flexDirection={'column'}>
+            <Text width="5rem;">Username</Text>
+            <Input type="text" width="20rem" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
+        </Box>
+        <Box display="flex" alignItems={'center'} margin="auto" width="fit-content" flexDirection={'column'}>
+            <Text width="5rem;">Password</Text>
+            <Input type="password" width="20rem" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
+        </Box>
+        <Button display='flex' margin='1rem auto' width={'fit-content'} >Submit</Button>
     </Container>
   )
 }
